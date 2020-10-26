8500cfa4da09f0b548e32ce14aa8826c5831fef8
Schuansk Lucas Tenorio Torres <schuansk@hotmail.com.br>
2020-10-26 09:07:01 -0300

    inicio layout cart

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
 export default function Cart() {
+
+
+    async function handleAdd() {
+
+    }
+
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text>Carrinho</Text>
+            <Button title="add" onPress={handleAdd}/>
         </View>
     );
 }
